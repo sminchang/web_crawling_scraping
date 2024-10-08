9400f67ceff9100678df1e0651976aea9403910d
Sung Minchang <58043537+sminchang@users.noreply.github.com>
2024-10-08 17:44:11 +0900

    Add files via upload

---
+++ pdf_scrapping/step1.pdf_text.py
@@ -0,0 +1,104 @@
+# spire.pdf는 정확도가 높지만 유료 API로 무료 버전에서는 페이지 갯수가 제한되어있다.
+
+# PyMuPDF, PyPDF2 모두 텍스트 내 공백 문자를 생략하고, 최상단에 위치한 세부사업 번호와 제목을 중간에 추출하는 문제가 있었다.
+# PyMuPDF는 텍스트 내 개행 문자를 기준으로 개행하는 것으로 보인다.
+# PyPDF2는 페이지 레이아웃을 기준으로 개행하는 것으로 보인다.
+
+# pdfplumber는 간혹 특정 형식에서 공백을 생략하거나, 리스트를 개행없이 이어버리는 경우가 있었지만 가공하면 쓸만해보였다.
+
+# 텍스트 추출에 있어, pdfminer가 원본 형식을 가장 잘 추출하는 것 같아 최종 사용하였다.
+# .six는 파이썬 2와 3사이의 호환성을 제공한다. 필요한 경우 추가한다.
+
+
+from pdfminer.high_level import extract_text
+
+def pdf_to_text(pdf_path, output_file_path):
+    # PDF 파일에서 텍스트 추출
+    text = extract_text(pdf_path)
+    
+    # 추출한 텍스트를 텍스트 파일로 저장
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+    print(f"텍스트가 '{output_file_path}'에 저장되었습니다.")
+
+
+# 추출할 PDF 경로, 저장할 파일 경로
+pdf_path = '2023_overPaging.pdf'
+output_file_path = 'output.txt'
+
+#실행
+pdf_to_text(pdf_path, output_file_path)
+
+
+#--------------------------------------------------------------------
+
+# import fitz
+
+# def extract_text_from_pdf(pdf_path, output_file_path):
+#     doc = fitz.open(pdf_path)
+    
+#     # 전체 텍스트를 저장할 변수
+#     full_text = ""
+    
+#     # 모든 페이지에서 텍스트 추출
+#     for page in doc:
+#         full_text += page.get_text()
+    
+#     with open(output_file_path, 'w', encoding='utf-8') as f:
+#         f.write(full_text)
+    
+#     print(f"텍스트가 '{output_file_path}'에 저장되었습니다.")
+
+
+# pdf_path = "2023_사업별 세부설명자료-A.pdf"
+# output_file_path = "output2.txt"
+
+# extract_text_from_pdf(pdf_path, output_file_path)
+
+#--------------------------------------------------------------------
+
+# import PyPDF2
+
+# def pdf_to_text(pdf_path, output_file_path):
+
+#     with open(pdf_path, 'rb') as pdf_file:
+#         pdf_reader = PyPDF2.PdfReader(pdf_file)
+        
+#         text = ''
+#         for page in pdf_reader.pages:
+#             text += page.extract_text() + '\n'  # 페이지별로 텍스트 추출하여 추가
+
+#     with open(output_file_path, 'w', encoding='utf-8') as f:
+#         f.write(text)
+
+#     print(f"텍스트가 '{output_file_path}'에 저장되었습니다.")
+
+
+# pdf_path = '2023_사업별 세부설명자료-A.pdf'
+# output_file_path = 'output3.txt'
+
+# pdf_to_text(pdf_path, output_file_path)
+
+#--------------------------------------------------------------------
+
+# import pdfplumber
+
+# def extract_text_from_pdf(pdf_file_path, output_file_path):
+
+#     with pdfplumber.open(pdf_file_path) as pdf:
+#         text = ""
+
+#         for page in pdf.pages:
+#             text += page.extract_text() + "\n\n"  # 각 페이지의 텍스트를 추가하고 줄바꿈
+
+#     with open(output_file_path, 'w', encoding='utf-8') as file:
+#         file.write(text)
+
+#     print(f"텍스트가 '{output_file_path}'에 저장되었습니다.")
+
+
+# pdf_path = '2023_사업별 세부설명자료-A.pdf'
+# output_file_path = 'output4.txt'
+
+# extracted_text = extract_text_from_pdf(pdf_path, output_file_path)
---
+++ pdf_scrapping/step2.pdf_title.py
@@ -0,0 +1,36 @@
+#pdf 내 문건을 분류하는 기준이 되는 세부사업 번호와 제목을 추출하는 스크립트.
+#추가로 pdf 전체 페이지(1542)와 추출된 엑셀 행(1539) 사이의 오차를 확인하고
+    # 페이지 오버플로우된 3개 문건을 파악하여 반영하였다.
+
+from pdfminer.high_level import extract_text
+import re
+import pandas as pd
+
+def extract_text_to_file(pdf_path, output_file):
+    # PDF 파일에서 전체 텍스트를 추출
+    text = extract_text(pdf_path)
+    
+    # 세부사업 번호와 제목을 저장할 리스트
+    data = []
+    
+    # 정규 표현식, 추출할 형식 패턴 지정 후 해당 데이터들만 추출
+    pattern = r'(?:세부사업:\s*((?:\d|\w){4}-(?:\d|\w){3}-(?:\d|\w){4}-(?:\d|\w){4}-(?:\d|\w){4}))\s*\n+(.*?)\s*(?=회계연도\s*:)'
+    matches = re.findall(pattern, text, re.DOTALL)
+    
+    # 추출한 데이터를 분할하여 리스트에 저장
+    for match in matches:
+        business_number = match[0].strip()
+        title = match[1].strip().replace('\n', '')  # 제목이 길어서 개행된 경우도 처리
+        data.append([business_number, title])
+    
+    # DataFrame 생성하여 리스트 데이터를 엑셀 파일에 저장
+    df = pd.DataFrame(data, columns=['세부사업 번호', '제목'])
+    df.to_excel(output_file, index=False)
+    print(f"데이터가 '{output_file}'에 저장되었습니다.")
+
+
+pdf_path = "2023_사업별 세부설명자료.pdf"
+output_file = "extracted_data.xlsx"
+
+extract_text_to_file(pdf_path, output_file)
+
